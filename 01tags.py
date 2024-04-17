df9f7b9247ae583832023f2243d60c2991849eda
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-17 10:45:40 +0530

    write function to get tags that has class bu not id

--- 01tags.py
+++ 01tags.py
@@ -19,3 +19,11 @@ ulTag.append(liTag)
 soup.html.body.insert(0, ulTag)
 with open("modified.html", "w") as f:
     f.write(str(soup))
+
+def has_class_but_not_id(tag):
+    return tag.has_attr("class") and not tag.has_attr("id")    
+
+results = soup.find_all(has_class_but_not_id)
+
+for r in results:
+    print(r, "/n")
