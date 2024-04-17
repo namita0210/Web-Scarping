360499656bf3b86e04431aaf15dee574f4ca7512
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-17 10:37:49 +0530

    lear to add an html file with specified tags

--- 01tags.py
+++ 01tags.py
@@ -5,3 +5,17 @@ with open("sample.html" , "r") as f:
     html_doc = f.read()
 
 soup = BeautifulSoup(html_doc , 'html.parser')    
+#print(soup.prettify())
+
+
+ulTag = soup.new_tag("ul")
+liTag = soup.new_tag("li")
+ulTag.append(liTag)
+
+liTag = soup.new_tag("li")
+liTag.string="About"
+ulTag.append(liTag)
+
+soup.html.body.insert(0, ulTag)
+with open("modified.html", "w") as f:
+    f.write(str(soup))
