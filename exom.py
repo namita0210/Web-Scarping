97f4f5f8c629da9cba38f1bc8da6a276afb3bf0b
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-19 12:48:50 +0530

    failed

--- exom.py
+++ exom.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://timesofindia.indiatimes.com/city/delhi"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+url = "https://www.meesho.com/search?q=iphone&searchType=manual&searchIdentifier=text_search"
 
-r = requests.get(url, headers=headers)
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+r = requests.get(url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
-print(soup.prettify())
+spans = soup.select("p.sc-eDvSVe gQDOBc NewProductCardstyled__StyledDesktopProductTitle-sc-6y2tys-5 ejhQZU NewProductCardstyled__StyledDesktopProductTitle-sc-6y2tys-5 ejhQZU")
+print(spans)
