3a179d977416568a7c00eaba0f3c4ac063012910
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-21 12:06:43 +0530

    load the webpade

--- Tutorial2/corey.py
+++ Tutorial2/corey.py
@@ -2,12 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://www.patreon.com/coreyms'
-path = 'Tutorial2/corey.html'
+path = 'Tutorial2/copyIndexSexy.html'
 
-def saveToFile(url, path):
-    r = requests.get(url)
-    with open (path , "w", encoding='UTF -8') as f:
-        f.write(r.text)
+r = requests.get(url)
+print(r.text)
 
-#saveToFile(url,path)        
+with open (r.text) as f:
+    soup = BeautifulSoup(f, 'lxml')
+    print(soup.prettify())
+ 
+
 
