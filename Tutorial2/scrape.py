e6421ed7315b3e89995154fb7809eb452cc3c6df
namita0210 <namita008mtaids22@igdtuw.ac.in>
2024-04-21 11:07:45 +0530

    Get the contents of the we page to scrape with different methods

--- Tutorial2/scrape.py
+++ Tutorial2/scrape.py
@@ -4,9 +4,18 @@ import requests
 url = 'http://127.0.0.1:5500/Tutorial2/index.html'
 path = 'Tutorial2/copyIndex.html'
 
-def saveFile(url , path):
+def saveFile(url , path): #Save url as file and then get its contents
     r = requests.get(url)
     with open (path , "w" , encoding='UTF-8') as f:
         f.write(r.text)
 
-saveFile(url , path)
+#saveFile(url , path)
+
+with open('index.html') as f: #Get the contents from html file with lxml parser
+    soup = BeautifulSoup(f, 'lxml')
+
+#print(soup)
+
+r = requests.get(url)  #Get the contents from url
+#print(r.text)
+
